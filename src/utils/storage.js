02eeac04001f945d7d6668aa556072059636ba7b
Vladimir <pestov.web@yandex.ru>
2025-02-10 16:12:56 +0300

    feat: file storage

---
+++ src/utils/storage.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../../uploads'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
+  },
+});
+
+module.exports = storage;
